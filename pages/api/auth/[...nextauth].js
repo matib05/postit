a0a8c013e6652f93e77cde339423def4d1fdb4f7
matib05 <matib05@gmail.com>
2023-05-16 02:20:36 -0400

    adding server side session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,12 +3,17 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../prisma/client";
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+const adapter = PrismaAdapter(prisma);
+
+export const authOptions = {
+  secret: process.env.AUTH_SECRET,
+  adapter: adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+}
+
+export default NextAuth(authOptions);
